9bb04eab6b341aa3d303dd4ca973c02edf7a0d75
chennapavansai <chennapavansai@gmail.com>
2026-08-21 13:55:00 +0530

    updated code

--- backend/server.js
+++ backend/server.js
@@ -41,6 +41,13 @@ app.use(errorHandler);
 
 const PORT = process.env.PORT || 5000;
 
+// app.listen(PORT, () => {
+//   console.log(`[Express Server]: Running in ${process.env.NODE_ENV || 'development'} mode on port ${PORT}`);
+// });
+
+
 app.listen(PORT, () => {
   console.log(`[Express Server]: Running in ${process.env.NODE_ENV || 'development'} mode on port ${PORT}`);
 });
+
+export default app;
